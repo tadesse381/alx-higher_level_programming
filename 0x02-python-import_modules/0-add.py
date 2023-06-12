7b792afc84e278a793a62f00b34eb50f0ee7d7c4
Tadesse Kebede <taddeekb@gmail.com>
2023-06-12 04:17:18 -0700

    0-add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == "__main__":
+    """
+
+    print sum of x and y
+
+    """
+    x = 1
+    y = 2
+    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
