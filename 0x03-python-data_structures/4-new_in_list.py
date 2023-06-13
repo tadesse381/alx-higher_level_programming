990c80af87b9ff153be29954d81598b6514b465c
Tadesse Kebede <taddeekb@gmail.com>
2023-06-12 23:53:04 -0700

    nwlst

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpy = my_list.copy()
+
+    if idx < 0 or idx >= len(my_list):
+        return cpy
+
+    cpy[idx] = element
+    return cpy
